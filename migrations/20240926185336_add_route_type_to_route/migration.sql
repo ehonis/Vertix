712e69896288c1c0db87dabdf0e9e8b23d95d0a3
ehonis <ethanhonisb@gmail.com>
2024-09-29 16:21:13 -0400

    add navbar and add dummy graphs

---
+++ migrations/20240926185336_add_route_type_to_route/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Route" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "setDate" TIMESTAMP(3) NOT NULL,
+    "grade" TEXT NOT NULL,
+
+    CONSTRAINT "Route_pkey" PRIMARY KEY ("id")
+);
