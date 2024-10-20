7a97f20654bc60e5ef9ef0db3ff5be3389f023c1
ehonis <ethanhonisb@gmail.com>
2024-10-20 15:45:28 -0400

    fix some deployment buga

--- app/edit/page.jsx
+++ app/edit/page.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Suspense } from 'react';
+
 import RoutePanels from '../ui/edit/route-panels';
 
 const getRoutes = async () => {
@@ -10,8 +10,7 @@ const getRoutes = async () => {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-        },
-        cache: 'no-store', // Move cache inside the options object
+        }, // Move cache inside the options object
       }
     );
 
@@ -24,42 +23,48 @@ const getRoutes = async () => {
 export default async function Page() {
   const routes = await getRoutes();
 
-  const boulderRoutes = routes.data.filter((route) => route.type === 'boulder');
-  const ropeRoutes = routes.data.filter((route) => route.type === 'rope');
+  try {
+    const boulderRoutes = routes.data.filter(
+      (route) => route.type === 'boulder'
+    );
+    const ropeRoutes = routes.data.filter((route) => route.type === 'rope');
 
-  return (
-    <>
-      <div className="flex justify-between w-full p-5">
-        <h1 className="text-white text-3xl font-bold">Edit Routes</h1>
-        <Link href={'./edit/new_route'}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="#4ade80"
-            className="size-14"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        </Link>
-      </div>
-      <div className="bg-bg1 m-5 rounded p-5">
-        <h2 className="text-white text-2xl font-extrabold px-5">Ropes</h2>
-        <div className="flex p-5">
-          <RoutePanels routes={ropeRoutes} />
+    return (
+      <>
+        <div className="flex justify-between w-full p-5">
+          <h1 className="text-white text-3xl font-bold">Edit Routes</h1>
+          <Link href={'./edit/new_route'}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#4ade80"
+              className="size-14"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </Link>
         </div>
-      </div>
-      <div className="bg-bg1 m-5 rounded p-5">
-        <h2 className="text-white text-2xl font-extrabold px-5">Boulders</h2>
-        <div className="flex gap-5 p-5">
-          <RoutePanels routes={boulderRoutes} />
+        <div className="bg-bg1 m-5 rounded p-5">
+          <h2 className="text-white text-2xl font-extrabold px-5">Ropes</h2>
+          <div className="flex p-5">
+            <RoutePanels routes={ropeRoutes} />
+          </div>
         </div>
-      </div>
-    </>
-  );
+        <div className="bg-bg1 m-5 rounded p-5">
+          <h2 className="text-white text-2xl font-extrabold px-5">Boulders</h2>
+          <div className="flex gap-5 p-5">
+            <RoutePanels routes={boulderRoutes} />
+          </div>
+        </div>
+      </>
+    );
+  } catch {
+    return <div>failed to get any routes</div>;
+  }
 }
