c33231a438f0e512ffc08f96d55b3f365e543be6
ehonis <ethanhonisb@gmail.com>
2024-10-20 20:36:06 -0400

    data cache fix 7 test

--- app/api/get-route/route.js
+++ app/api/get-route/route.js
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const data = await prisma.Route.findMany();
   return NextResponse.json({ data });
