1a8afa53c07bc9ed902903f2d5f39b0e9e8ae1d9
ehonis <ethanhonisb@gmail.com>
2024-10-31 11:28:22 -0400

    add google auth

--- auth.js
+++ auth.js
@@ -1,14 +1,10 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { hashPassword } from '@/utils/password';
-import { getUserFromDb } from './utils/db';
-import { ZodError } from 'zod';
-import { signInSchema } from './lib/zod';
 import Github from 'next-auth/providers/github';
 import prisma from '@/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import Google from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Github],
+  providers: [Github, Google],
 });
