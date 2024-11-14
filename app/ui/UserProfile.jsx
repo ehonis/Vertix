386a71d055bd76a9838923ae89e3776d3bf1db63
ehonis <ethanhonisb@gmail.com>
2024-11-14 14:17:39 -0500

    redo image handling for presentation

--- app/ui/UserProfile.jsx
+++ app/ui/UserProfile.jsx
@@ -7,6 +7,7 @@ export default function UserProfile({ user }) {
   const [isProfilePopUp, setIsProfilePopUp] = useState(false);
   const profileRef = useRef(null);
   const buttonRef = useRef(null);
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
@@ -51,34 +52,16 @@ export default function UserProfile({ user }) {
   } else {
     return (
       <div>
-        {!user.image ? (
-          <Image
-            src={user.image}
-            alt="User Avatar"
-            height={50}
-            width={50}
-            className="size-9 rounded-full"
-            onClick={handleClick}
-            ref={buttonRef}
-          />
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="white"
-            className="size-8 cursor-pointer"
-            onClick={handleClick}
-            ref={buttonRef}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        )}
+        <Image
+          src={user.image}
+          alt="User Avatar"
+          height={50}
+          width={50}
+          className="size-9 rounded-full"
+          onClick={handleClick}
+          ref={buttonRef}
+        />
+
         <div
           ref={profileRef}
           className={clsx(
