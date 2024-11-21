1e9b32621f368b81b9a8d1cbb9ed68262380380a
ehonis <ethanhonisb@gmail.com>
2024-11-21 13:12:38 -0500

    commit for v14 of nextjs if v15 breaks anything and i want to revert

--- app/routes/[slug]/loading.jsx
+++ app/routes/[slug]/loading.jsx
@@ -1,3 +1,9 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center top-1/2">
+      <div className="text-white font-barlow font-bold text-3xl ">
+        Loading...
+      </div>
+    </div>
+  );
 }
