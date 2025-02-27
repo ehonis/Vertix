d4ee46a5fee0f7ca28fd833d52af69064a963e2e
ehonis <ethanhonisb@gmail.com>
2025-02-27 13:35:38 -0500

    add climbers section to mixer manager

--- app/ui/admin/competitions/mixer/individualMixerManagerPage/individual-comp-page-load.jsx
+++ app/ui/admin/competitions/mixer/individualMixerManagerPage/individual-comp-page-load.jsx
@@ -25,6 +25,7 @@ export default function IndividualCompPageLoad({
     }))
   );
   const [compDivisions, setCompDivisions] = useState(divisions);
+  const [compClimbers, setCompClimbers] = useState(climbers);
   const [selectedDate, setSelectedDate] = useState(
     compDay.toISOString().split('T')[0]
   );
@@ -39,6 +40,11 @@ export default function IndividualCompPageLoad({
   const [isEditRoutePopup, setIsEditRoutePopup] = useState(false);
   const [tempHolds, setTempHolds] = useState();
   const [tempRouteName, setTempRouteName] = useState('');
+
+  const [showAllClimbers, setShowAllClimbers] = useState(false);
+  const displayedClimbers = showAllClimbers
+    ? compClimbers
+    : compClimbers.slice(0, 10);
 
   const handleScoresAvailableButtonClick = () => {
     setIsScoresAvailableInfoPopUp(true);
@@ -279,6 +285,7 @@ export default function IndividualCompPageLoad({
           /> */}
         </div>
       </div>
+      {/* divisions */}
       <div>
         <h3 className="text-3xl mt-3">Divisions</h3>
         <div className="bg-bg2 flex-col gap-2 flex p-3 rounded w-full">
@@ -319,6 +326,7 @@ export default function IndividualCompPageLoad({
           )}
         </div>
       </div>
+      {/* routes */}
       <div>
         <h3 className="text-3xl mt-3">Routes</h3>
         <div className="bg-bg2 flex-col gap-2 flex p-3 rounded w-full">
@@ -366,6 +374,63 @@ export default function IndividualCompPageLoad({
                   </svg>
                 </button>
                 <p className="font-medium">Add Route</p>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+      <div>
+        <h3 className="text-3xl mt-3">Climbers</h3>
+        <div className="bg-bg2 flex-col gap-2 flex p-3 rounded w-full">
+          {compClimbers.length > 0 ? (
+            <div className="w-full flex-col flex gap-2">
+              {displayedClimbers.map((climber) => (
+                <button key={climber.id}>
+                  <div className="grid bg-bg1 grid-cols-2 items-center p-1 px-2 w-full rounded">
+                    <p className="text-xl justify-self-start truncate">
+                      {climber.name}
+                    </p>
+                    <p
+                      className={clsx(
+                        'text-base place-self-end font-normal',
+                        climber.entryMethod === 'APP'
+                          ? 'text-green-500'
+                          : 'text-white'
+                      )}
+                    >
+                      {climber.entryMethod}
+                    </p>
+                  </div>
+                </button>
+              ))}
+              <div className="flex w-full justify-between">
+                <div className="flex items-center gap-1">
+                  <button className="bg-green-400 p-1 rounded-full max-w-fit">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="size-7 "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                  </button>
+                  <p className="font-medium">Add Manual User</p>
+                </div>
+                <button
+                  className={'px-2 py-1 bg-blue-500 rounded'}
+                  onClick={() => setShowAllClimbers(!showAllClimbers)}
+                >
+                  {showAllClimbers ? 'Show Less' : 'Show More'}
+                </button>
               </div>
             </div>
           ) : (
