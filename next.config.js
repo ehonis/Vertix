1121203f5a7606049127e0ba3fb3d5f85fe6d88f
ehonis <ethanhonisb@gmail.com>
2025-03-07 22:55:52 -0500

    remove sentry

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/a/8jiyvthxbb/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**', // Match all paths under /u/
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
