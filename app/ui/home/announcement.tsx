a2005c55e35eebaf0aa484bc01b33ef219e08eab
ehonis <ethanhonisb@gmail.com>
2025-04-01 12:18:39 -0400

    fix for announcement on desktop view

--- app/ui/home/announcement.tsx
+++ app/ui/home/announcement.tsx
@@ -2,10 +2,7 @@ import Link from "next/link";
 
 export default function Announcement({}) {
   return (
-    <Link
-      href="/competitions/mixer/cm780exfs0002oi365hvzdtt2"
-      className=" rounded-lg md:w-md mt-20"
-    >
+    <Link href="/competitions/mixer/cm780exfs0002oi365hvzdtt2" className=" rounded-lg md:w-md ">
       <h2 className="font-barlow text-white text-sm font-semibold text-center ">
         ❗<span className="underline">Sign ups are now open for Spring Mixer 2025</span>❗
       </h2>
