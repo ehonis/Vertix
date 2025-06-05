d42009f332bd55ee026f27ba26b6ac450dbfa373
Ethan Honis <88252192+ehonis@users.noreply.github.com>
2025-06-05 14:58:27 -0400

    changed announcement text

--- app/ui/home/announcement.tsx
+++ app/ui/home/announcement.tsx
@@ -4,7 +4,7 @@ export default function Announcement({}) {
   return (
     <Link href="/competitions/mixer/cm780exfs0002oi365hvzdtt2" className=" rounded-lg md:w-md ">
       <h2 className="font-barlow text-white text-sm font-semibold text-center ">
-        ❗<span className="underline">Sign ups are now open for Spring Mixer 2025</span>❗
+        ❗<span className="underline">Route Tracking is live</span>❗
       </h2>
     </Link>
   );
